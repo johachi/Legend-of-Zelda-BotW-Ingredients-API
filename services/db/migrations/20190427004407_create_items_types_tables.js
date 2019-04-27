32622cbbaa56af9f3096501f157a50025cbcb373
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2019-04-27 11:50:11 +0900

    Migration file for TABLE item_types

---
+++ services/db/migrations/20190427004407_create_items_types_tables.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+	return knex.schema("item_types", (table) => {
+		table.increments().index();
+		table.string("type").notNullable();
+	})
+
+};
+
+
+
+exports.down = function(knex, Promise) {
+	
+};
